e46835edc683adb2c2cee51b90a51d10f4088ff5
Luis Carlos Núñez Guerra <luis.carlos.nunez.guerra@gmail.com>
2025-03-06 08:51:27 -0600

    master commit
    GOLEM v0.1

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim priority As String = ""
+        Console.WriteLine("---------------------------")
+        Console.WriteLine("GOLEM PRIORITY CHECK V.0.2")
+        Console.WriteLine("---------------------------")
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("What's the priority today?")
+        priority = Console.ReadLine()
+
+        Console.WriteLine("Ok, you told me the priority is: " & priority)
+        Console.ReadKey()
+    End Sub
+
+End Module
