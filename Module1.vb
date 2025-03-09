45cc66f92607d1855bd7dc8fc8d880c7e5b8ff06
Luis <buddynod32@hotmail.com>
2025-03-09 08:10:18 -0600

    dumping priority to file

--- Module1.vb
+++ Module1.vb
@@ -1,15 +1,26 @@
-﻿Module Module1
+Imports System.IO
+
+
+Module Module1
+
 
     Sub Main()
         Dim priority As String = ""
         Console.WriteLine("---------------------------")
-        Console.WriteLine("GOLEM PRIORITY CHECK V.0.2")
+        Console.WriteLine("GOLEM PRIORITY CHECK V.0.8.1")
         Console.WriteLine("---------------------------")
         Console.WriteLine("Hello World!")
         Console.WriteLine("What's the priority today?")
         priority = Console.ReadLine()
 
+	Dim todaySign = DateTime.Now.ToString("ddMMyyyy")
         Console.WriteLine("Ok, you told me the priority is: " & priority)
+        'TODO: Dump it to txt file in ddMMyyyy format
+        Dim outPath As String = "out_" & todaySign & ".txt"
+        Using writer As new StreamWriter(outPath, append:=False)
+            writer.WriteLine(todaySign & " " & "priority is: " & priority)
+        End Using
+	Console.WriteLine("Dumped priority succesfully")
         Console.ReadKey()
     End Sub
 
